ba861b69869f8af9e430cdd876b7acbcf123aa99
Hector Rojas Barcenas <hectorrojasbarcenas@MacBook-Air-de-Hector.local>
2023-01-15 17:28:44 -0600

    Create character controller

---
+++ src/controllers/character.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import { handleHttp } from "../utils/error.handler";
+
+const getCharacter = (req: Request, res: Response) => {
+  try {
+  } catch (e) {
+    handleHttp(res, "ERROR_GET_CHARACTER");
+  }
+};
+
+const getCharacters = (req: Request, res: Response) => {
+  try {
+  } catch (e) {
+    handleHttp(res, "ERROR_GET_CHARACTERS");
+  }
+};
+
+const updateCharacter = (req: Request, res: Response) => {
+  try {
+  } catch (e) {
+    handleHttp(res, "ERROR_UPDATE_CHARACTER");
+  }
+};
+
+const postCharacter = (req: Request, res: Response) => {
+  try {
+  } catch (e) {
+    handleHttp(res, "ERROR_POST_CHARACTER");
+  }
+};
+
+const deleteCharacter = (req: Request, res: Response) => {
+  try {
+  } catch (e) {
+    handleHttp(res, "ERROR_DELETE_CHARACTER");
+  }
+};
+
+export {
+  getCharacter,
+  getCharacters,
+  updateCharacter,
+  postCharacter,
+  deleteCharacter,
+};
