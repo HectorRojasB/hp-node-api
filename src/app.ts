17315179fc54f75b8667c5deeae79d3bae78e309
Hector Rojas Barcenas <hectorrojasbarcenas@MacBook-Air-de-Hector.local>
2023-01-15 14:44:58 -0600

    Include mongo settings

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { router } from "./routes";
+import db from "./config/mongo";
 
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
 app.use(router);
+db().then(() => console.log("DB connection ready"));
 app.listen(PORT, () => console.log("up and running!"));
